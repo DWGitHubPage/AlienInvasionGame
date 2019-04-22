25881166967c04623c19dc5aee1efe5bfec72ef9
Edie W <owl.robot@aol.com>
2019-04-22 15:44:09 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,6 @@ class Settings():
 
         # How quickly the alien point values increase
         self.score_scale = 1.5
-
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -45,5 +44,4 @@ class Settings():
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
-
         self.alien_points = int(self.alien_points * self.score_scale)
